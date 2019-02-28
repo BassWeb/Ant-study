a45a5176cf061ad6262372a6e7c9b1ad187dcff6
BassWeb <bassmonroe@qq.com>
2019-02-28 10:35:21 +0800

    hello

--- config/config.js
+++ config/config.js
@@ -1,13 +1,24 @@
 export default {
     plugins: [
         ['umi-plugin-react', {
-            // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+            antd: true,
+            dva: true
         }]
     ],
     routes: [
+        // {
+        //     path: '/Helloworld',
+        //     component: './helloworld/Helloworld'
+        // },
         {
-            path: 'Helloworld',
-            component: './helloworld/Helloworld'
+            path: '/',
+            component: '../layout',
+            routes: [
+                {
+                    path: 'Helloworld',
+                    component: './helloworld/Helloworld'
+                }
+            ]
         }
     ]
 }
